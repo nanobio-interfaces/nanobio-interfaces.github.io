02f581c72007e0fad20b45a5dfbaa28b1cada4a9
nanobio-interfaces <tiago.espinosa@ufcspa.edu.br>
2025-05-09 15:20:46 +0000

    Deploying to gh-pages from @ nanobio-interfaces/nanobio-interfaces.github.io@3c241a4f19044afc0fc4a2692a379d18763e452f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-sobre-o-nanobio",
+    title: "Sobre o NanoBio",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
