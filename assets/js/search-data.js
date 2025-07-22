e62787fbd7f72a6719518cd84b68f169ee8adc59
nanobio-interfaces <tiago.espinosa@ufcspa.edu.br>
2025-07-22 11:36:13 +0000

    Deploying to gh-pages from @ nanobio-interfaces/nanobio-interfaces.github.io@8ae3f2d65e3f71399e1b2e5cb9d36a9fb415436b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-linhas-de-pesquisa",
-          title: "Linhas de Pesquisa",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "Materials for courses you taught. Replace this text with your description.",
